70805e8a17bfbc416f6738c55c152b36ee7ffc7d
SongLong <songlong2205@gmail.com>
2022-10-04 16:24:00 +0700

    routes

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World12345!')
-})
+const adminRoute = require('./routes/admin');
+const userRoute = require('./routes/user');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.json());
+
+app.use(adminRoute);
+app.use(userRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
